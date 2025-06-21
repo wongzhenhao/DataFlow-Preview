daee35f26c920cfdc4aeecad01861698e2cd50a4
HeRunming <2100010636@stu.pku.edu.cn>
2025-06-21 21:26:09 +0800

    Add some registry

--- dataflow/operators/generate/__init__.py
+++ dataflow/operators/generate/__init__.py
@@ -3,6 +3,10 @@ from dataflow.utils.registry import LazyLoader
 
 cur_path = "dataflow/operators/generate/"
 _import_structure = {
+    "AnswerGenerator": (cur_path + "Reasoning/AnswerGenerator.py", "AnswerGenerator"),
+    "QuestionCategoryClassifier": (cur_path + "Reasoning/QuestionCategoryClassifier.py", "QuestionCategoryClassifier"),
+    "QuestionDifficultyClassifier": (cur_path + "Reasoning/QuestionDifficultyClassifier.py", "QuestionDifficultyClassifier"),
+    "QuestionGenerator": (cur_path + "Reasoning/QuestionGenerator.py", "QuestionGenerator"),
 }
 
 sys.modules[__name__] = LazyLoader(__name__, "dataflow/operators/generate/", _import_structure)
