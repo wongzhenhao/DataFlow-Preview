e955c74525df1cc8b7de6765834fd0893d2ee125
Sunnyhaze <mxch1122@126.com>
2025-06-25 23:32:30 +0800

    [update] refine relative path in test cases

--- test/test_ragkbcleaning.py
+++ test/test_ragkbcleaning.py
@@ -15,8 +15,8 @@ class KBCleaningPipeline():
     def __init__(self):
 
         self.storage = FileStorage(
-            first_entry_file_name="dataflow/example/KBCleaningPipeline/pdf_test.json",
-            cache_path="./.cache",
+            first_entry_file_name="../dataflow/example/KBCleaningPipeline/pdf_test.json",
+            cache_path="./cache",
             file_name_prefix="dataflow_cache_step",
             cache_type="jsonl",
         )
