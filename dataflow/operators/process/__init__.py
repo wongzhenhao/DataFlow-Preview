35f1fdb85f19a9cf8fc701aaa99fc38f5293d9f4
TheRoadQaQ <maluqaq@163.com>
2025-06-26 18:17:14 +0800

    agentic rag pipeline

--- dataflow/operators/process/__init__.py
+++ dataflow/operators/process/__init__.py
@@ -2,6 +2,7 @@ import sys
 from dataflow.utils.registry import LazyLoader
 from .Reasoning import *
 from .GeneralText import *
+from .AgenticRAG import *
 cur_path = "dataflow/operators/process/"
 _import_structure = {
     "AnswerFormatterFilter": (cur_path + "Reasoning/AnswerFormatterFilter.py", "AnswerFormatterFilter"),
@@ -11,6 +12,7 @@ _import_structure = {
     "AnswerPipelineRoot": (cur_path + "Reasoning/AnswerPipelineRoot.py", "AnswerPipelineRoot"),
     "AnswerTokenLengthFilter": (cur_path + "Reasoning/AnswerTokenLengthFilter.py", "AnswerTokenLengthFilter"),
     "QuestionFilter": (cur_path + "Reasoning/QuestionFilter.py", "QuestionFilter"),
+    'ContentChooser': (cur_path + "AgenticRAG/ContentChooser.py", "ContentChooser"),
 }
 
 sys.modules[__name__] = LazyLoader(__name__, "dataflow/operators/process/", _import_structure)
